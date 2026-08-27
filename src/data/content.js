e924cc22a1bb9520b214cfaf014def3b428ded42
Preetinderjeet Singh <singhpreetinder229@gmail.com>
2026-08-27 13:04:59 +0530

    adding some links

--- src/data/content.js
+++ src/data/content.js
@@ -42,6 +42,7 @@ export const projects = [
     image: null,
     github: "",
     loom: "#",
+    Link:"#",
     tag: "systems",
   },
   {
@@ -51,6 +52,7 @@ export const projects = [
     image: null,
     github: "#",
     loom: "#",
+    Link: "#",
     tag: "ml",
   },
   {
